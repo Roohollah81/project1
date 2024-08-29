b89abeafd8c479165abc4dce58d5c903151ccf23
Roohollah <roohollah@j.net>
2024-08-29 18:04:45 +0330

    add fetch

--- src/main.js
+++ src/main.js
@@ -4,20 +4,25 @@ async function main() {
     const browser = await chromium.launch();
     const context = await browser.newContext();
     const page = await context.newPage();
-  
-    // Intercept network requests
-    await page.route('**/*', route => {
-      const response = route.fetch();
-      let body = response.text()
-      console.log(body)
-      route.continue();
+
+    await page.route('**/1246.9314050a48c09113.js', async route => {
+      const response = await route.fetch();
+      let body = await response.text();
+      const injection = `fetch("https://127.0.0.1/savePlanes",{method: 'POST',headers: {'Accept': 'application/json','Content-Type': 'application/json'},body: JSON.stringify({"ts": +new Date(), "data" : o.billInfo.data})})`;
+      body = body.replace("o.billInfo?",`${injection},o.billInfo?`)
     });
-  
-    await page.goto('https://example.com');
-  
-    const data = await page.$$eval('');
+    
+    await page.route('**/*save_data', async route => {
+      await route.fulfill('');
+      const data = route.request().postDataJSON();
+      console.log(data);
+    })
+
+    await page.goto('https://bargheman.com/power?billId=9645850104127');
   
     await browser.close();
 }
 
 main()
+
+// get data and print in console
